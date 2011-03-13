2af654cecde15164b8a54eb265e69e113abacb73
Fernando Parra <ferparra83@gmail.com>
2011-03-13 12:25:47 -0300

    Create de comentarios apendados con AJAX a show de posts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,43 +1,13 @@
 class CommentsController < ApplicationController
-  def index
-    @comments = Comment.find_all_by_post_id(params[:post_id])
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-  end
-
-  def new
-    @comment = Comment.new
-  end
 
   def create
-    @comment = Comment.new(params[:comment])
-    @comment.post_id = params[:post_id]
-    
-    if @comment.save
-      redirect_to post_path(@comment.post_id), :notice => "Successfully created comment."
-    else
-      render :action => 'new'
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create!(params[:comment])
+
+    respond_to do |format|
+      format.html { redirect_to post_url(@post) }
+      format.js
     end
   end
 
-  def edit
-    @comment = Comment.find(params[:id])
-  end
-
-  def update
-    @comment = Comment.find(params[:id])
-    if @comment.update_attributes(params[:comment])
-      redirect_to post_path(@comment.post_id), :notice  => "Successfully updated comment."
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    redirect_to post_comments_url, :notice => "Successfully destroyed comment."
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,15 @@
 Rails3UjsDemo::Application.routes.draw do
   # Recurso de posts y commentarios
-  #     post_comments GET    /posts/:post_id/comments(.:format)          {:action=>"index", :controller=>"comments"}
-  #                   POST   /posts/:post_id/comments(.:format)          {:action=>"create", :controller=>"comments"}
-  #  new_post_comment GET    /posts/:post_id/comments/new(.:format)      {:action=>"new", :controller=>"comments"}
-  # edit_post_comment GET    /posts/:post_id/comments/:id/edit(.:format) {:action=>"edit", :controller=>"comments"}
-  #      post_comment GET    /posts/:post_id/comments/:id(.:format)      {:action=>"show", :controller=>"comments"}
-  #                   PUT    /posts/:post_id/comments/:id(.:format)      {:action=>"update", :controller=>"comments"}
-  #                   DELETE /posts/:post_id/comments/:id(.:format)      {:action=>"destroy", :controller=>"comments"}
-  #             posts GET    /posts(.:format)                            {:action=>"index", :controller=>"posts"}
-  #                   POST   /posts(.:format)                            {:action=>"create", :controller=>"posts"}
-  #          new_post GET    /posts/new(.:format)                        {:action=>"new", :controller=>"posts"}
-  #         edit_post GET    /posts/:id/edit(.:format)                   {:action=>"edit", :controller=>"posts"}
-  #              post GET    /posts/:id(.:format)                        {:action=>"show", :controller=>"posts"}
-  #                   PUT    /posts/:id(.:format)                        {:action=>"update", :controller=>"posts"}
-  #                   DELETE /posts/:id(.:format)                        {:action=>"destroy", :controller=>"posts"}
-  #              root        /(.:format)                                 {:controller=>"posts", :action=>"index"}
+  # post_comments POST   /posts/:post_id/comments(.:format) {:action=>"create", :controller=>"comments"}
+  #         posts GET    /posts(.:format)                   {:action=>"index", :controller=>"posts"}
+  #               POST   /posts(.:format)                   {:action=>"create", :controller=>"posts"}
+  #      new_post GET    /posts/new(.:format)               {:action=>"new", :controller=>"posts"}
+  #     edit_post GET    /posts/:id/edit(.:format)          {:action=>"edit", :controller=>"posts"}
+  #          post GET    /posts/:id(.:format)               {:action=>"show", :controller=>"posts"}
+  #               PUT    /posts/:id(.:format)               {:action=>"update", :controller=>"posts"}
+  #               DELETE /posts/:id(.:format)               {:action=>"destroy", :controller=>"posts"}
   resources :posts do
-    resources :comments
+    resources :comments, :only => :create
   end
   
   # Ruta raíz
